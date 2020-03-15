e4f8266b841062c7194de430a9b7b0659e1020c1
Inrix <Inrixia@gmail.com>
2020-03-16 09:03:25 +1300

    Changed exports to not directly touch modules.

--- helloWorld.js
+++ helloWorld.js
@@ -1,9 +1,12 @@
-const thread = require('./lib/Thread.js').Child
+const { Parent, Child } = require('./lib/Thread.js')
+
+const otherThread = new Parent('./logThings.js')
 
 const helloWorld = async () => {
-	console.log('wait require')
-	await thread.require('./logThings.js')
-	return `Hello World ${await thread.threads['./logThings.js'].doubleIt(1)}`
+	// return 'Hello World'
+	return await otherThread.doubleIt(1)
+	// await thread.require('./logThings.js')
+	// return `Hello World ${await thread.threads['./logThings.js'].doubleIt(1)}`
 }
 
 module.exports = { helloWorld }
