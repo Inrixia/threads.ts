36218d93201170b3f99d86d6aa0211c28ac531bc
Inrix <Inrixia@gmail.com>
2020-03-16 13:01:26 +1300

    Fixes for DistributedThreads
    Added global sharedArrayBufferSize for defining the size of all new sharedArrayBuffers.
    Added support for empty thread creation (for DistributedParent extends).
    Distributed now extends Thread to remove duplicate code & fix self calls from children.
    Non async functions are now wrapped in an async function so either can be used.
    Parents now support options.sharedArrayBuffer to be given.
    Changed function calls to all use "call" instead of "function".
    Referenced DistributedParent threads should now show correct values stored in sharedArrayBuffer.
    ThreadStore no longer overwrites values on creation.
    Removed _loadBalance and moved code directly into calls as its never used elsewhere.

--- helloWorld.js
+++ helloWorld.js
@@ -3,7 +3,8 @@ const { Parent, Child } = require('./lib/Thread.js')
 const otherThread = new Parent('./logThings.js')
 
 const helloWorld = async () => {
-	// return 'Hello World'
+	console.log(await Child.test())
+	// return 'Hello Parent'
 	return await otherThread.doubleIt(1)
 	// await thread.require('./logThings.js')
 	// return `Hello World ${await thread.threads['./logThings.js'].doubleIt(1)}`
--- threadTest.js
+++ threadTest.js
@@ -2,6 +2,8 @@ const { Parent, DistributedParent } = require('./lib/Thread.js')
 
 
 ;(async () => {
-	const helloWorldThread = new Parent('helloWorld.js')
+	const helloWorldThread = new DistributedParent('helloWorld.js')
 	console.log(await helloWorldThread.helloWorld())
 })().catch(console.log)
+
+module.exports = { test: () => 'Hello Child' }
