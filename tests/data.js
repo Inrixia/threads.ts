775a0defca7d41dbdd8054c19011493b40004958
Inrix <Inrixia@gmail.com>
2020-10-06 08:50:10 +1300

    Added back thread data passing as a option

---
+++ tests/data.js
@@ -0,0 +1,1 @@
+module.exports._data = async () => module.parent.thread.data
---
+++ tests/data.test.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const { Parent, ParentPool, Thread } = require('../index.js')
+
+test('Checks data is correctly passed to a thread', () => {
+	const data = { a: "Hello World" }
+	const dataThread = new Parent(path.join(__dirname, './data.js'), { name: 'dataThread', data })
+	return expect(dataThread._data()).resolves.toMatchObject(data);
+});
+
+test('Checks data is correctly passed to a thread', () => {
+	const data = { a: "Hello Worlds" }
+	const dataThread = new ParentPool(path.join(__dirname, './data.js'), { name: 'dataThreadPool', data })
+	return expect(dataThread._data()).resolves.toMatchObject(data);
+});
