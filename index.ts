439bc831a68c8059687499b1da733ab9b879c882
Inrix <Inrixia@gmail.com>
2021-03-05 02:01:47 +1300

    Moved index.ts to root & Updated configs

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * as Parent from "./src/Parent";
+export * as Thread from "./src/Thread";
+export * as ThreadStore from "./src/ThreadStore";
+export * from "./src/Types";
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * as Parent from "./Parent";
-export * as Thread from "./Thread";
-export * as ThreadStore from "./ThreadStore";
-export * as Types from "./Types";
